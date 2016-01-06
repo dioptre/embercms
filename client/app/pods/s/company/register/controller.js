37de6dc2ed60a18dbb52ed01a6940c4c22417de8
andy grosser <dioptre@gmail.com>
2016-01-05 19:57:59 -0800

    updated queryparams

--- client/app/pods/s/company/register/controller.js
+++ client/app/pods/s/company/register/controller.js
@@ -1,7 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	tabs: [ {id: 'a', title: 'First'},
-        {id: 'b', title: 'Second'},
-        {id: 'c', title: 'Third'} ],
+	queryParams: ['tab'],
+	tab: '0',
+	tabs: [ {id: '0', title: 'Contact'},
+        {id: '1', title: 'Expertise'},
+        {id: '2', title: 'Awards'} ],
 });
