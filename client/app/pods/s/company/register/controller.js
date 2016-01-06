d67540d37d288dfbe5f42ecf1267094123298435
andy grosser <dioptre@gmail.com>
2016-01-05 19:32:35 -0800

    preparing for real work

---
+++ client/app/pods/s/company/register/controller.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+	tabs: [ {id: 'a', title: 'First'},
+        {id: 'b', title: 'Second'},
+        {id: 'c', title: 'Third'} ],
+});
